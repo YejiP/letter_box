9774e52e63f01d47a6d4fd30623abb192f21d2fa
YejiP <21500280@handong.edu>
2022-06-21 17:49:48 +0900

    userauth

--- notes/urls.py
+++ notes/urls.py
@@ -13,12 +13,16 @@ urlpatterns = [
     path('new/', views.new, name='new'),
     path('create/', views.create, name='create'),
 
+    path('bulk_create/', views.bulk_create, name='bulk_create'),
+
     path('<int:note_id>/delete/', views.delete, name='delete'),
     path('signup/', views.signup, name='signup'),
     path('signup_new/', views.signup_new, name='signup_new'),
 
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('login_view/', views.login_view, name='login_view'),
+    path('logout_view/', views.logout_view, name='logout_view'),
 
      # ex: /notes/1/
+    #이렇게하면 리소스로 헷갈릴수있어서 절대일케하면안된다.
+     # notes/bulk_create/3/ => notes/bulk_create?limit=3
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Notes
-from .models import New_user
-
-from django.http import Http404
+from .models import User
+from django.http import Http404, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
-
+from django.contrib.auth import get_user, logout
+from django.utils import timezone
+from django.views.decorators.csrf import csrf_exempt
+import json
 
 """
 index : display notes by time
@@ -17,39 +19,40 @@ To get from a URL to a view, Django uses what are known as ‘URLconfs’. A URL
 https://docs.djangoproject.com/en/4.0/topics/db/queries/
 """
 
-user=None
+
+current_user=None
 def signup_new(request):
     return render(request,'notes/signup.html')
 
 def signup(request):
-    New_user.objects.create(user_id = request.POST['user_id'],user_pwd=request.POST['user_pwd'])
+    User.objects.create_user(username = request.POST['user_id'],password=request.POST['user_pwd'],last_login=timezone.now(),is_superuser=True)
     return redirect('index')
 
-def login(request):
-    global user
-    uid= request.POST['user_id']
-    upwd = request.POST['user_pwd']
-    try:
-        if New_user.objects.filter(user_id = uid).get(user_pwd=upwd):
-            print("logged in")
-            user= New_user.objects.get(user_id = uid)
-            return redirect('index')
-        else:
-            print("no")
-    except New_user.DoesNotExist:
-        raise Http404("Note does not exist")
+def login_view(request):
+    #signup 에서 create_user 아니라 create 써서 error나는 거였다.
+    username= request.POST['user_id']
+    password = request.POST['user_pwd']
 
-def logout(request):
-    global user
-    user=None
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        global current_user
+        login(request, user)
+        return redirect('index')
+    else:
+        # Return an 'invalid login' error message.
+        raise Http404("invalid login")
+
+        
+
+def logout_view(request):
+    logout(request)
     return redirect('index')
 
 
 #get
 def index(request):
-    global user
     notesList = Notes.objects.order_by('title')
-    context = {'latest_Note_list': notesList, 'current_user' : user}
+    context = {'latest_Note_list': notesList, 'current_user' : get_user(request)}
     return render(request, 'notes/index.html', context)
 
 #get
@@ -58,7 +61,7 @@ def detail(request, note_id):
         note = Notes.objects.get(pk=note_id)
     except Notes.DoesNotExist:
         raise Http404("Note does not exist")
-    return render(request, 'notes/detail.html', {'notes': note, 'current_user':user})
+    return render(request, 'notes/detail.html', {'notes': note, 'current_user':get_user(request)})
 
 #get
 def new(request):
@@ -66,7 +69,8 @@ def new(request):
 
 #post
 def create(request):
-    note = Notes.objects.create(user=user,title = request.POST.get('note_title'),text=request.POST.get('note_text'))
+    #나중에 삭제 user! user= User.objects.all().get(id=3)
+    note = Notes.objects.create(user=get_user(request),title = request.POST.get('note_title'),text=request.POST.get('note_text'))
     return render(request, 'notes/detail.html', {'notes': note})
 
 #form
@@ -89,4 +93,23 @@ def delete(request, note_id):
     except Notes.DoesNotExist:
         raise Http404("Note does not exist")
     notesList = Notes.objects.order_by('title')
-    return redirect('index') 
+    return redirect('index')
+    
+
+
+#post endpoint, and create HttpResponse notes, limit is the number of notes.
+#I cannot call request.post/get together in the same endpoint.
+##10000명이 동시에 note create -> 빨리만드려고??????1초안ㅇㅔ
+@csrf_exempt
+def bulk_create(request):
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    data = json.loads(request.body.decode('utf-8'))
+    print(data)
+
+    limit=data['limit']
+    user= User.objects.all().get(id=3)
+    for i in range(limit):
+        print(i)
+    Notes.objects.create(user = get_user(request), title = data['title'], text =data['text']) 
+    #Notes.objects.bulk_create()
+    return HttpResponse(content= 200)
